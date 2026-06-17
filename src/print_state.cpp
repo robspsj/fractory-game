58633cb3bc7a7ad5ce0c84c91295f8c01a44d2fc
Roberto Proença <roberto.proenca@rovio.com>
2026-06-18 00:04:11 +0200

    Refactor printCell to use Cell type with switch; fix subData buffer overflow for size 4-5 subgrids

--- src/print_state.cpp
+++ src/print_state.cpp
@@ -17,26 +17,31 @@ static const char* _itemColors[GameModel::ELEMS] = {
     "\033[1;34m", "\033[1;31m", "\033[1;32m", "\033[1;33m", "\033[1;35m"
 };
 
-static void printCell(std::ostream& os, int arrayIdx, int id, int count, int subgridIdx, int idxWidth) {
+static void printCell(std::ostream& os, int arrayIdx, const Cell& cell, int idxWidth, int subgridLetterIdx = -1) {
     int contentWidth = idxWidth + 4;
-    if (id >= 0) {
-        os << "[" << std::setw(idxWidth) << arrayIdx << ":"
-           << _itemColors[id] << id << "\033[0m" << ":" << count << "]";
-    } else if (id == -1) {
-        std::ostringstream content;
-        content << std::setw(idxWidth) << arrayIdx << ":";
-        int pad = contentWidth - (int)content.tellp();
-        for (int i = 0; i < pad; i++) content << "_";
-        os << "[" << content.str() << "]";
-    } else {
-        os << "{" << std::setw(idxWidth) << arrayIdx << ":\033[1m#\033[0m:" << (char)('b' + subgridIdx) << "}";
+    switch (cell.type) {
+        case CellType::ITEM:
+            os << "[" << std::setw(idxWidth) << arrayIdx << ":"
+               << _itemColors[cell.data.item.id] << cell.data.item.id << "\033[0m" << ":"
+               << cell.data.item.count << "]";
+            break;
+        case CellType::GRID: {
+            char letter = subgridLetterIdx >= 0 ? (char)('b' + subgridLetterIdx) : '?';
+            os << "{" << std::setw(idxWidth) << arrayIdx << ":\033[1m#\033[0m:" << letter << "}";
+            break;
+        }
+        default: {
+            std::ostringstream content;
+            content << std::setw(idxWidth) << arrayIdx << ":";
+            int pad = contentWidth - (int)content.tellp();
+            for (int i = 0; i < pad; i++) content << "_";
+            os << "[" << content.str() << "]";
+            break;
+        }
     }
 }
 
 void printState(const GameModel& model) {
-    int data[GameModel::GRID * GameModel::GRID * 2];
-    model.getFullState(data);
-
     int total = model.totalNodes();
     int idxWidth = digitWidth(total);
 
@@ -51,51 +56,46 @@ void printState(const GameModel& model) {
         std::cout << "Drag:  None" << std::endl;
     }
 
+    int gridSize = GameModel::GRID;
+
     std::cout << std::endl << "Root:" << std::endl;
     std::cout << "{" << std::setw(idxWidth) << 0 << ":\033[1m#\033[0m:a}" << std::endl;
 
     int maxSubgridIdx = -1;
-    int first = 1;
+    int subgridCount = 0;
+
+    for (int i = 0; i < gridSize; ++i) {
+        for (int j = 0; j < gridSize; ++j) {
+            if (model.node(model.rootChild(i, j)).type == CellType::GRID) {
+                subgridCount++;
+            }
+        }
+    }
+    maxSubgridIdx = subgridCount - 1;
 
     std::cout << std::endl << "Subgrid a:" << std::endl;
-    for (int i = 0; i < GameModel::GRID; ++i) {
-        for (int j = 0; j < GameModel::GRID; ++j) {
-            int idx = first + i * GameModel::GRID + j;
-            int id = data[(i * GameModel::GRID + j) * 2];
-            int count = data[(i * GameModel::GRID + j) * 2 + 1];
-            if (id == -2) {
-                if (count > maxSubgridIdx) maxSubgridIdx = count;
-                printCell(std::cout, idx, id, count, count, idxWidth);
-            } else {
-                printCell(std::cout, idx, id, count, -1, idxWidth);
-            }
+    subgridCount = 0;
+    for (int i = 0; i < gridSize; ++i) {
+        for (int j = 0; j < gridSize; ++j) {
+            int idx = model.rootChild(i, j);
+            const Cell& cell = model.node(idx);
+            int sgIdx = (cell.type == CellType::GRID) ? subgridCount++ : -1;
+            printCell(std::cout, idx, cell, idxWidth, sgIdx);
             std::cout << " ";
         }
         std::cout << std::endl;
     }
 
     for (int s = 0; s <= maxSubgridIdx; s++) {
-        char letter = 'a' + s;
-        std::cout << std::endl << "Subgrid " << (char)(letter + 1) << ":" << std::endl;
-        int subData[9 * 2];
+        std::cout << std::endl << "Subgrid " << (char)('b' + s) << ":" << std::endl;
+        std::vector<int> subData(GameModel::GRID * GameModel::GRID * 2);
         int subSize;
-        int subFirst = model.getSubgridState(s, subData, subSize);
+        int subFirst = model.getSubgridState(s, subData.data(), subSize);
         for (int i = 0; i < subSize; ++i) {
-            for (int j = 0; j < subSize; j++) {
+            for (int j = 0; j < subSize; ++j) {
                 int cellIdx = subFirst + i * subSize + j;
-                int dataIdx = (i * subSize + j) * 2;
-                int id = subData[dataIdx];
-                int count = subData[dataIdx + 1];
-                if (id == -2) {
-                    std::ostringstream content;
-                    content << std::setw(idxWidth) << cellIdx << ":?";
-                    int pad = (idxWidth + 4) - (int)content.tellp();
-                    for (int i = 0; i < pad; i++) content << " ";
-                    std::cout << "{" << content.str() << "} ";
-                } else {
-                    printCell(std::cout, cellIdx, id, count, -1, idxWidth);
-                    std::cout << " ";
-                }
+                printCell(std::cout, cellIdx, model.node(cellIdx), idxWidth);
+                std::cout << " ";
             }
             std::cout << std::endl;
         }
