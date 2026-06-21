52bcc0d7d5d0b4c6bc13d7c96f84a2aa5bd867d0
Roberto Proença <roberto.proenca@rovio.com>
2026-06-21 15:21:21 +0200

    printState: remove setw on root index output

--- src/print_state.cpp
+++ src/print_state.cpp
@@ -59,7 +59,7 @@ void printState(const GameModel &model) {
   }
 
   std::cout << std::endl << "Root:" << std::endl;
-  std::cout << "{" << std::setw(idxWidth) << 0 << ":\033[1m#\033[0m:a}"
+  std::cout << "{"<< "\033[1m#\033[0m:a}"
             << std::endl;
 
   int subgridCount = -1;
