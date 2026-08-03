99659fcd613873e4c685c9af583a3535cedcefd3
Roberto Proença <roberto.proenca@rovio.com>
2026-08-03 15:13:57 +0200

    feat: add STATION/RESERVED cell types with StationData, ReservedData, and byteSize tracking

--- src/game/game_model.hpp
+++ src/game/game_model.hpp
@@ -3,7 +3,9 @@
 #include "interact_config.hpp"
 #include <vector>
 
-enum class CellType { EMPTY, ITEM, GRID };
+enum class CellType { EMPTY, ITEM, GRID, STATION, RESERVED };
+
+enum class ReserveRole { NONE, INPUT, OUTPUT, BUFFER };
 
 struct ItemData {
   int id;
@@ -15,18 +17,49 @@ struct GridData {
   int gridDimension;
 };
 
+struct StationData {
+  int recipeIndex;
+  int progress;     // ticks remaining, 0 = idle
+  int sizeR, sizeC; // dimensions
+};
+
+struct ReservedData {
+  int anchorIndex;
+  ReserveRole role;
+  int itemId;
+  int itemCount;
+};
+
 struct CellContent {
   CellType type = CellType::EMPTY;
+  int byteSize = 0;
   union {
     ItemData item;
     GridData grid;
+    StationData station;
+    ReservedData reserved;
   } data;
+
+  CellContent() : type(CellType::EMPTY), byteSize(0) {
+    data.item = {-1, 0};
+  }
 };
 
 struct Cell {
   int parentId = -1;
   CellContent content;
 };
+
+inline int calcByteSize(CellType type) {
+  switch (type) {
+    case CellType::EMPTY:    return 9;
+    case CellType::ITEM:     return 13;
+    case CellType::GRID:     return 13;
+    case CellType::STATION:  return 17;
+    case CellType::RESERVED: return 18;
+  }
+  return 0;
+}
 
 class GameModel {
 public:
@@ -68,12 +101,16 @@ public:
   void drop(int nodeIndex);
   void cancelDrag();
   void interact(int nodeIndex);
+  bool placeStation(int gridIndex, int recipeId);
+  void removeStation(int anchorIndex);
 
   void tick();
   void tickCell(int idx);
   void tickEmpty(int idx);
   void tickItem(int idx);
+  void tickStation(int idx);
   void tickGrid(int idx);
+  void tryStartStation(int anchorIdx);
   void setFullState(int *inData);
   void getFullState(int *outData) const;
   int getSubgridState(int subgridSeqIndex, int *outData, int &outSize) const;
